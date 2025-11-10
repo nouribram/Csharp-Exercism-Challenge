1c15ddf352d50ff91b515c81cbb44d9d83c85237
nouribram <nouribram3@gmail.com>
2025-11-10 05:28:43 +0200

    Day #01

---
+++ Day#01 Hello World/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+"Hello, World!" is the traditional first program for 
+beginning programming in a new language or environment.
+The objectives are simple:
+Modify the provided code so that it produces the string "Hello, World!".
+Run the test suite and make sure that it succeeds.
+Submit your solution and check it at the website.
+If everything goes well, you will be ready to fetch your first real exercise.
+*/
+
+
+using System;
+
+public class HelloWorld
+{
+    // Must match the exact name that the test expects
+    public static string Hello()
+    {
+        return "Hello, World!";
+    }
+}
